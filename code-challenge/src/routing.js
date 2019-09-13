4bceb14c074f60dac94f138981d6b1ff535b0ad5
snntaylan <sinan.taylan@outlook.com>
2019-09-13 17:23:39 +0200

    added routing

---
+++ code-challenge/src/routing.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Search from "./components/search";
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import Notfound from "./components/404";
+import App from "./App";
+
+export const routing = (
+    <Router>
+        <div>
+            <Route path="/" exact component={App} />
+            <Route path="/search" component={Search} />
+            <Switch>
+                <Route path='/404' exact component={Notfound} />
+            </Switch>
+        </div>
+    </Router>
+);
