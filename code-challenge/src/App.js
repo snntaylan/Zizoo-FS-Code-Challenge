d4668883c31450c6aaedec044e26957c9ea1acb3
snntaylan <sinan.taylan@outlook.com>
2019-09-13 17:41:28 +0200

    added components

--- code-challenge/src/App.js
+++ code-challenge/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
+import Search from "./components/Search";
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-
+        <Search></Search>
       </header>
     </div>
   );
